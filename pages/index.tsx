6b7b040de2747b28410b2a9a66412f982bf80643
Arturo Macias <arturomaciasmx@gmail.com>
2023-03-12 18:47:16 -0700

    movies list component and endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Billboard from "@/components/Billboard";
 import NavigationBar from "@/components/NavigationBar";
+import MovieList from "@/components/MovieList";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
+import useMoviesList from "@/hooks/useMoviesList";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,9 +25,12 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMoviesList();
   return (
     <>
       <NavigationBar />
+      <Billboard />
+      <MovieList title="Last Movies" data={movies} />
       <Billboard />
     </>
   );
